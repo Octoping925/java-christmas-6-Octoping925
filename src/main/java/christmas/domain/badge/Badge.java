6401b2380ac07a488a2a5adb7178a0a62fb96768
Octoping <mooner925@gmail.com>
2023-11-15 00:26:57 +0900

    feat: Badge에 name 추가

--- src/main/java/christmas/domain/badge/Badge.java
+++ src/main/java/christmas/domain/badge/Badge.java
@@ -1,9 +1,19 @@
 package christmas.domain.badge;
 
 public enum Badge {
-    TREE,
-    STAR,
-    NONE,
-    SANTA,
+    TREE("트리"),
+    STAR("산타"),
+    NONE("없음"),
+    SANTA("산타"),
     ;
+
+    private final String name;
+
+    Badge(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
